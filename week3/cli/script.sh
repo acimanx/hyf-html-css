c24f3956022247e4fc9cc1a35ffbedc2bd31cb2b
andrew <39067270+acimanx@users.noreply.github.com>
2018-06-21 12:37:59 +0200

    Update script.sh

--- week3/cli/script.sh
+++ week3/cli/script.sh
@@ -1,28 +1,33 @@
 #!/bin/sh
+#shebang determines target shell
 directory="script_1.0";
 fl1="blanc";
 fl2="greetings.txt";
 fl3="pets.txt";
 fl4="commands.txt";
 fl5="lovelyCommands.txt"
+#defined variables
 
 if [ ! -d "$directory" ]; then
 mkdir -p -- "$directory" && cd -P -- "$directory"
+#if directory exist proceed; if not exist - throw a promt with choice
 echo directory "$directory" was created.
 
-touch ${fl1}
+touch "$fl1"
 echo file "$fl1" created.
 
-cat > $fl2 << EOL
+cat > "$fl2" << EOL
 1.Hello
 2.Hello
 3.Hello
 4.Hello
 5.Hello
 EOL
+#trick to do it multiline
 echo text was written into "$fl2" file.
 
 for i in $(seq 1 5); do cat "$fl2" >> "$i.txt"; done
+#because shebang "sh" supports only seq http://tldp.org/HOWTO/Bash-Prog-Intro-HOWTO-7.html
 echo content of "$fl2" was copied to 5 different files.
 
 cat > "$fl3" << EOL
@@ -45,11 +50,11 @@ echo "$fl3" and "$fl4" were sorted. "$fl5" was created.
 else
 
 while true; do
-printf "It seems that script was already executed\\nPress \e[31m \e[5mR \e[0mto remove existing folder.\\nPress \e[32m \e[5mQ \e[0mto quit."
+printf "It seems that script was already executed\\nPress \\e[31m \\e[5mR \\e[0mto remove existing folder.\\nPress \\e[32m \\e[5mQ \\e[0mto quit."
 read -r yn
-case $yn in
+case "$yn" in
 [Qq]* ) echo " Bye-bye! "; sleep 1; exit;;
-[Rr]* ) echo " Folder was removed. Please, rerun script again."; rm -r $directory; sleep 1; exit;;
+[Rr]* ) echo " Folder was removed. Please, rerun script again."; rm -r "$directory"; sleep 1; exit;;
 * ) echo "Please answer R or Q.";;
 esac
 
